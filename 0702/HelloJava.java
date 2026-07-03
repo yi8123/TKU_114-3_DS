d6fd3b3f79bfc4638e54a0fb0a1d69ab0f0e2829
50811123jo-design <50811123jo@gmail.com>
2026-07-03 14:04:37 +0800

    Add 0702 Java practice

--- 0702/HelloJava.java
+++ 0702/HelloJava.java
@@ -1,5 +1,7 @@
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("My name is yi8123");
+        System.out.println("This is my Java practice.");
     }
 }
