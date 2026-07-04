5f14a6c99ac4a82711493958db2035fdc5aeb3d0
50811123jo-design <50811123jo@gmail.com>
2026-07-04 16:30:51 +0800

    Add 0702 Java practice

---
+++ 0702/UserInfo.java
@@ -0,0 +1,15 @@
+public class UserInfo {
+    public static void main(String[] args) {
+        
+        String userName = "Amy";
+        String userId = "412345678";
+        String department = "Information Management";
+        int expectedScore = 90;
+
+        
+        System.out.println("Name: " + userName);
+        System.out.println("ID: " + userId);
+        System.out.println("Department: " + department);
+        System.out.println("Expected Score: " + expectedScore);
+    }
+}
