297710bfc76a9e7f59e0206063a8ca194ed0f2fc
50811123jo-design <50811123jo@gmail.com>
2026-07-04 16:04:21 +0800

    Add 0702 Java practice

---
+++ 0702/DailyRoutine.java
@@ -0,0 +1,9 @@
+public class DailyRoutine {
+    public static void main(String[] args) {
+        System.out.println("Wake up");
+        System.out.println("Brush teeth");
+        System.out.println("Go to class");
+    }
+}
+    
+
---
+++ 0702/ProfileCard.java
@@ -0,0 +1,11 @@
+public class ProfileCard {
+    public static void main(String[] args) {
+        String name = "Amy";
+        String department = "Information Management";
+        String favoriteCourse = "Programming";
+
+        System.out.println("Name: " + name);
+        System.out.println("Department:" + department);
+        System.out.println("Favorite Course:" + favoriteCourse);
+    }
+}
