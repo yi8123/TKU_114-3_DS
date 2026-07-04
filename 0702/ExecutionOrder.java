43f6b970ea2be2c79d4cfa12ece7a7b7a800757f
50811123jo-design <50811123jo@gmail.com>
2026-07-04 16:05:38 +0800

    Add 0702 Java practice

---
+++ 0702/ExecutionOrder.java
@@ -0,0 +1,7 @@
+public class ExecutionOrder {
+    public static void main(String[] args) {
+        System.out.println("Step 1");
+        System.out.println("Step 2");
+        System.out.println("Step 3");
+    }
+}
