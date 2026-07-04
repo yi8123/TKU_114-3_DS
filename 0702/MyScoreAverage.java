aa73a937188e1f807b8948e902fcbdb0fc8a8690
50811123jo-design <50811123jo@gmail.com>
2026-07-04 17:23:01 +0800

    Add 0702 Java practice

---
+++ 0702/MyScoreAverage.java
@@ -0,0 +1,11 @@
+public class MyScoreAverage {
+    public static void main(String[] args) {
+        int Java = 85;
+        int English = 78;
+        int Math = 92;
+
+        double average = (Java + English + Math) / 3.0;
+
+        System.out.println("Average score: " + average);
+    }
+}
