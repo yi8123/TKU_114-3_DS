453c73830280e6a0e785c40face1c47f424e1fa3
50811123jo-design <50811123jo@gmail.com>
2026-07-04 17:14:17 +0800

    Add 0702 Java practice

---
+++ 0702/CourseInfo.java
@@ -0,0 +1,16 @@
+public class CourseInfo {
+    public static void main(String[] args) {
+
+        
+        String courseName = "Programming";
+        int credits = 2;
+        double hours = 8.0;
+        boolean required = true;
+
+        
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Credits: " + credits);
+        System.out.println("Hours: " + hours);
+        System.out.println("Required: " + required);
+    }
+}
