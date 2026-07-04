241e04f59b43eef9a0106af42d18de7632e63907
50811123jo-design <50811123jo@gmail.com>
2026-07-04 16:16:45 +0800

    Add 0702 Java practice

--- 0702/ProfileCard.java
+++ 0702/ProfileCard.java
@@ -1,11 +1,13 @@
 public class ProfileCard {
     public static void main(String[] args) {
+         //用來顯示個人資料
         String name = "Amy";
         String department = "Information Management";
         String favoriteCourse = "Programming";
 
+        //輸出名字
         System.out.println("Name: " + name);
-        System.out.println("Department:" + department);
-        System.out.println("Favorite Course:" + favoriteCourse);
+        System.out.println("Department: " + department);
+        System.out.println("Favorite Course: " + favoriteCourse);
     }
 }
