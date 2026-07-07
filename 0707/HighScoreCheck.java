5cd691e7bd0483d9139332d32f56695473aac617
yi8123 <50811123jo@gmail.com>
2026-07-07 10:26:12 +0800

    Add 0707 condition and loop practice

--- 0707/HighScoreCheck.java
+++ 0707/HighScoreCheck.java
@@ -1,10 +1,11 @@
 public class HighScoreCheck {
     public static void main(String[] args){
-        int score 100;
+        int score = 90;
 
         System.out.println("Score: " + score);
 
         if (score >= 90) {
             System.out.println("Excellent");
     } 
+}
 }
