ac3252db6de5b13fbb493795df67983a99f8d078
yi8123 <50811123jo@gmail.com>
2026-07-07 10:18:50 +0800

    Add 0707 condition and loop practice

---
+++ 0707/HighScoreCheck.java
@@ -0,0 +1,10 @@
+public class HighScoreCheck {
+    public static void main(String[] args){
+        int score 100;
+
+        System.out.println("Score: " + score);
+
+        if (score >= 90) {
+            System.out.println("Excellent");
+    } 
+}
---
+++ 0707/LoginCheck.java
@@ -0,0 +1,6 @@
+public class LoginCheck {
+    public static void main(String[] args){
+        String username = "admin";
+        String password = "1234";
+    }
+}
