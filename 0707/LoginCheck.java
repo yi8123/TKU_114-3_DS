97f8bc9ffea4b8c5ce4b2dfeabec995fbda112cb
yi8123 <50811123jo@gmail.com>
2026-07-07 10:39:24 +0800

    Add 0707 condition and loop practice

--- 0707/LoginCheck.java
+++ 0707/LoginCheck.java
@@ -2,5 +2,9 @@ public class LoginCheck {
     public static void main(String[] args){
         String username = "admin";
         String password = "1234";
+
+        boolean 
+        String inputUsername = "admin";
+        String inputPassword = "1234";
     }
 }
