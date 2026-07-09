3dbce4f980bf320f9fc56b5c3667dfa4bd5512b1
yi8123 <50811123jo@gmail.com>
2026-07-09 10:34:21 +0800

    dd 0709 method practice

---
+++ 0709/WelcomeMethod.java
@@ -0,0 +1,10 @@
+public class WelcomeMethod {
+    public static void main(String[] args) {
+        printWelcome();   
+    }
+
+    public static void printWelcome(){
+        System.out.println("Welcome to Java");
+        System.out.println("Practice methods");
+    }
+}
