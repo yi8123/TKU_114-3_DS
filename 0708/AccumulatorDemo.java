02ca22553169df584be16a8ba94271c303dff958
yi8123 <50811123jo@gmail.com>
2026-07-09 09:59:26 +0800

    Add 0708 nested loop and menu practice

---
+++ 0708/AccumulatorDemo.java
@@ -0,0 +1,11 @@
+public class AccumulatorDemo {
+    public static void main(String[] args) {
+        int total = 0;
+
+        total += 80;
+        total += 90;
+        total += 75;
+
+        System.out.println("Total: " + total);
+    }
+}
