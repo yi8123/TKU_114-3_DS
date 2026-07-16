da63ca7f8c9744444e3e4fa725c579624e72013c
50811123jo-design <50811123jo@gmail.com>
2026-07-16 13:54:11 +0800

    Add midterm_exam

--- midterm_exam/Q01_ParkingFeeFix.java
+++ midterm_exam/Q01_ParkingFeeFix.java
@@ -1,26 +1,19 @@
 public class Q01_ParkingFeeFix {
-    public static int calculateFee(int minutes) {
-
-        if (minutes < 0) {
-        return -1;
+    public static void main(String[] args) {
+        int[] testMinutes = {-20, 30, 31, 60, 61, 120, 121, 420};
+        for (int minutes : testMinutes) {
+        int fee = calculateFee(minutes);
+        System.out.println("停車 " + minutes + " 分鐘，費用：" + fee + " 元");
         }
-
+    }
+    public static int calculateFee(int minutes) {
         if (minutes <= 30) {
         return 0;
         }
-
-        int fee;
-
         if (minutes <= 120) {
-        fee = ((minutes - 31) / 30 + 1) * 20;
-        } else {
-        fee = 60 + ((minutes - 121) / 60 + 1) * 30;
+        return ((minutes - 30) / 30) * 20;
         }
-
-        if (fee > 180) {
-        fee = 180;
-        }
-
+        int fee = 60 + ((minutes - 120) / 60) * 30;
         return fee;
     }
 }
